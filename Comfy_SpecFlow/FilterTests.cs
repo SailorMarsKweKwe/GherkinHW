55d60b5561d4732646c3d2061c2a47c58f15f36c
innasukhina <innasukhina@gmail.com>
2022-02-02 21:35:50 +0200

    добавляю фалы по автоматизации геркина

---
+++ Comfy_SpecFlow/FilterTests.cs
@@ -0,0 +1,16 @@
+using OpenQA;
+using OpenQA.Selenium;
+using TechTalk.SpecFlow;
+
+namespace Comfy_SpecFlow
+{
+    [Binding]
+    public class FilterTests
+    {
+        [Given(@"User on the homepage")]
+        public void GivenUserOnTheHomepage()
+        {
+
+        }
+    }
+}
